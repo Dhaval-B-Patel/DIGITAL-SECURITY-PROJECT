b392980da2661fc31e1b60ea901927619897665f
Alhwirif <oozz.2014@gmail.com>
2020-03-14 17:30:18 -0600

    Update pixelImage.py

--- Requirement 5/code/pixelImage.py
+++ Requirement 5/code/pixelImage.py
@@ -1,16 +1,19 @@
 from PIL import Image
 import random
 
-x = 150;
-y = 150;
+x = 0;
+y = 0;
 value = 1;
 j = 0;
+t = 0;
 im = Image.open('test.jpg') # Can be many different formats.
 pix = im.load()
 vav = pix[x,y] #RGP values
 list = [0,0,0,0]
-newVav = (0,0,0,0)
+newVav = (0,0,0,0);
+
 print (im.size)  # Get the width and hight of the image for iterating over
+print (newVav)
 #print (vav)
 #print (vav[0])
 #print (vav[1])
@@ -24,10 +27,10 @@ print (list)
 random.shuffle(list) # shuffle a values ( must be a list )
 print (list)
 
-#for i in list: # changing the list values to tuples
-#	newVav[j] = i
-#	j +=1
-#print (newVav)  
+for i in list: # changing the list values to tuples
+	newVav[t] = i
+	t +=1
+print (newVav)  
 
 #pix[x,y] = newVav # Set the RGBA Value of the image (tuple)
 #im.save('alive_parrot.png')  # Save the modified pixels as .png
